329a02927e4cd867ef96402a89787cf8456612b7
Bernat <bernat.gili@gmail.com>
2023-01-19 18:33:49 +0100

    Start building fight script

--- Pokemon_combat/pokecombat.py
+++ Pokemon_combat/pokecombat.py
@@ -17,8 +17,37 @@ def any_player_pokemon_lives(player_profile):
     return sum([pokemon["current_health"] for pokemon in player_profile["pokemon_inventory"]]) > 0
 
 
+def choose_pokemon(player_profile):
+    index = 1
+    print("\nQué pokemon de tu equipo quieres que luche?")
+    for pokemon in player_profile["pokemon_inventory"]:
+        print(index, pokemon["name"])
+        index += 1
+    choosen_pokemon = player_profile["pokemon_inventory"][int(input()) - 1]
+    return choosen_pokemon
+
+
+def choose_player_attack(player_pokemon):
+    index = 1
+    for attack in player_pokemon["attacks"]:
+        if int(attack["min_level"]) <= player_pokemon["level"] != 0:
+            print(index, attack["name"])
+
+    #return input()
+
+
+def choose_enemy_attack(enemy_pokemon):
+    for attack in enemy_pokemon["attacks"]:
+        if int(attack["min_level"]) >= enemy_pokemon["level"]:
+            return attack
+
+
 def fight(player_profile, enemy_pokemon):
-    
+    print("Que empiece el combate!\n Te vas a enfrentar a {}.".format(enemy_pokemon["name"]))
+    player_pokemon = choose_pokemon(player_profile)
+    player_attack = choose_player_attack(player_pokemon)
+    enemy_attack = random.choice(choose_enemy_attack(enemy_pokemon))
+
 
 def main():
     pokemon_list = get_all_pokemons()
